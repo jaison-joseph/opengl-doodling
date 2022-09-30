7a4113eeaba711bd8d41da30702187d5a51ef2d2
Jaison Joseph <jaison@Jaisoon.localdomain>
2022-09-30 15:08:54 -0400

    triangle

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -50,6 +50,142 @@ int main() {
     // 'registering' the function to be called when the window is resized
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // vertices for the triangle
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    // creating a vertex buffer object; stores vertices in GPU mem
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    // binding the buffer to the GL_ARRAY_BUFFER, the type of a vertex buffer
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    //copy the defined buffer to the buffer's mem
+    // the 4th param tells how we want the graphics card to manage the data 
+    // there's 3 options: 
+    // GL_STREAM_DRAW: set once used rarely
+    // GL_STATIC_DRAW: set once used frequenty
+    // GL_DYNAMIC_DRAW: set multiple and used frequently)
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // so now we have stored the vertex data in GPU memory, mananged by a vertex buffer object called VBO
+
+    // the vertex shader
+
+    // vec3 is a vector that stores 3 floats (hence the suffix 3)
+    // the input vertex is declared in the vec3 vector
+    // To set the output of the vertex shader we have to assign the position data to the predefined gl_Position
+
+    // this shader does no processing on the input; it simply forwards the input vector to the output
+    // the 4th param in the vec$ argument is the position of the vertex attribute in the shader
+    const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main() {\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+    // to compile the shader, we call a function to create a 'vertex' kind of shader, then attach the code for th shaer to it and compile
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    // glShaderSource(shader obj, # of strings, strings, NULL);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // checking to see if the compilation was successful
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // fragment shader
+
+    // very similar to the vertex shader
+
+    // source for the fragment shader
+    // the point of this basic fragment shader is simply setting the colors of the pixels, we choose orange
+    const char* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main() {\n"
+    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
+    // create fragment shader obejct, attach code for our frag shader, compile
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // shader program: the final linked version of multiple shaders, we activate this program to render objects
+
+    // creating a program obj
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    // attaching our shaders to the program and then linking the program
+    // note that the order in which we attach is important, vertex, followed by fragment
+    // improper order will give a linking error
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+
+    // checking to see if the linking was successful
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // since we linked the vertex and fragment shaders to the program object, we can discard them
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // by now, we have sent the input vertex data to the GPU, along with instructions on how to process the vertex data
+    // within the shaders. 
+    // We still need to instruct openGL how to interpret vertex data in memory, connect data to the shader attributes
+
+    // we will create a Vertex Array Object (VAO)
+    // stores the configs to draw an object into a container. Precisely
+    // 1. Calls to glEnableVertexAttribArray or glDisableVertexAttribArray.
+    // 2. Vertex attribute configurations via glVertexAttribPointer.
+    // 3. Vertex buffer objects associated with vertex attributes by calls to glVertexAttribPointer.
+    // can be bound like the VBO 
+    // allows us to just configure the vertex attribute pointers once, subsequent calls are replaced by binding the VAO
+
+    // creating a VAO
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    // binding the VAO
+    glBindVertexArray(VAO);
+    
+    // copy vertices array in a buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // 2. then set the vertex attributes pointers
+    /*
+    * the arguments to glVertexAttribPointer are
+    * a. the location of the position vertex attribute in the shader (the 4th argument in instantiating the vec4 in the vertex shader
+    * b. size of the vertex attribute
+    * c. type of data
+    * d. to normalize or not; if datatype is int, byte, then true; else false
+    * e. offset for telling where the position data begins in the buffer (it's at the start, so offset is 0)
+    */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // the code that renders and draws the triangle will be in the render loop
+
+
+    
     // render loop
 
     /*
@@ -68,8 +204,14 @@ int main() {
     {
         processInput(window);
 
+        // background color
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // triangle drawing
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -79,14 +221,3 @@ int main() {
     glfwTerminate();
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- README.md
+++ README.md
@@ -9,13 +9,49 @@
 * The state of OpenGL is called the 'OpenGL context'
 * State is changed by setting options, manipulating buffers and rendering using the current context
 * You will see state-changing functions and state-using functions
+* Everything in OpenGL is 3D. Transforming the 3D coordinates to 2D pixels is done by the graphics pipeline
+* The pipeline has seversl stages, each one requiring as input the result of the previous part. The parts are parallelizable, so they run on GPU cores, each one called a shader.
+* We can write our own shader using the OpenGL shading language(GLSL)
+* There's 6 stages to the pipeline:
+    * Vertex shader
+        * Vertex is a collection of data for a 3D coordinates
+        * input: a single vertex
+        * does: transforms 3D coordinates (Normalized Device Coordinates to screen space coordinates) and basic processing on vertex attributes?
+    * Shape assembly
+        * input: all the vertices form vertex shader
+        * does: assembles in the given "primitive shape"
+    * Geometry shader:
+        * input: vertices that form a primitive
+        * does: createe new shapes by emitting new vertices, forming new primitives
+    * Rasterization
+        * input: result from geo. shader
+        * does: 
+            * map the primitives to screen pixels, creating 'fragments' for the fragment shader
+            * clipping: remove fragments outside screen view, for better performance
+    * Fragment shader
+        * frag shader contains info about scene to calculate pixel color like light, shadow, light color, etc. 
+        * does: 
+            * calculate final color of a pixel
+    * Tests & blending?
+        * checks depth (stencil) value of a fragment, discards if it is behind another fragment
+        * checks 'alpha values' (opacity) and blends the objects accordingly
+* For most cases we only work with vertex and fragment shader
+* The geometry shader is optional and usually left to its default shader. 
+* In modern OpenGL, we need to define a vertex and fragment shader
 
 ## How to get this working on your machine
 
 * For all platforms: refer [this](https://learnopengl.com/Getting-started/Creating-a-window)
-* For a summary of what I installed on my machine, here's a list:
+* For a summary of what I installed on my machine to start writing OpenGL, here's a list:
     * [GLFW](https://www.glfw.org/): multi-platform library to create an OpenGL context, receive user input
     * [CMake](https://cmake.org/): We will use this to build solutions files for Visual Studio of the GLFW source code
     * [Visual Studio](https://visualstudio.microsoft.com/): nice powerful C++ editor. For this, we use it to build the solution file built by CMake into a library that can be linked by our OpenGL application. Also using it as the editor for this project.
     * [GLAD](): a library that finds the OpenGL functions that are used and stores them as function pointers. As for why this needs to be done, refer the GLAD section of [this](https://learnopengl.com/Getting-started/Creating-a-window)
 
+## How to OpenGL
+
+* The explanation is left as comments in **`ConsoleApplication1.cpp`**
+
+## Things I've drawn
+
+* Look at the images directory in the repo
